5c9aaf64e357c4a35814b7c4b072593d1a99fa0e
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2015-06-11 05:47:34 +0000

    Decided against that last trailing newline in help text.

--- vee.c
+++ vee.c
@@ -34,8 +34,7 @@ char const helpText[] =
  "  -h, --help              Print this help text and exit.\n"
  "  -i, --ignore-interrupts Ignore interrupt signals (SIGINT)\n"
  "\n"
- " * File descriptors are expected to be positive integers.\n"
- "\n";
+ " * File descriptors are expected to be positive integers.\n";
 
 char const unrecognizedOption[] = "vee: Unrecognized option: ";
 
