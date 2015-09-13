9cabbde24516ba49ff227a1e40ec7f5cce73dcb1
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2015-09-13 05:43:37 +0000

    Updated copyright date stamp and semver number.

--- vee.c
+++ vee.c
@@ -1,6 +1,6 @@
 /*
- * vee 3.4.2
- * Copyright (C) Alexander Kozhevnikov <mentalisttraceur@gmail.com> 2015-08-24;
+ * vee 3.4.3
+ * Copyright (C) Alexander Kozhevnikov <mentalisttraceur@gmail.com> 2015-09-13;
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public Licence as published by
