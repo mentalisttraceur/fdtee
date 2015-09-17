29c01c3e62d24866b7817604857308e8663e2a1a
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2015-09-17 09:11:07 +0000

    Updating copyright datetime and version number.

--- vee.c
+++ vee.c
@@ -1,6 +1,6 @@
 /*
- * vee 3.4.4
- * Copyright (C) Alexander Kozhevnikov <mentalisttraceur@gmail.com> 2015-09-13;
+ * vee 3.4.5
+ * Copyright (C) Alexander Kozhevnikov <mentalisttraceur@gmail.com> 2015-09-17;
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public Licence as published by
