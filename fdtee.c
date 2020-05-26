7430597b7796bbdcaec2082d900c4d77ea1eccac
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2020-05-26 01:36:39 +0000

    Relicense to 0BSD

--- fdtee.c
+++ fdtee.c
@@ -1,22 +1,17 @@
-/*
- * fdtee 0.0.0
- * Copyright (C) 2015-12-21 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public Licence as published by
- * the Free Software Foundation, either version 3 of the licence or,
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for details.
- * 
- * You should've received a copy of the GNU General Public License
- * with this program. If not, see <http://www.gnu.org/licences/>,
- * or write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330 Boston MA 02111-1307 USA.
- */
+/*****************************************************************************\
+ * Copyright 2015 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+\*****************************************************************************/
 
 #define _POSIX_C_SOURCE 1
 
