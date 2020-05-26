a3294d12896702905098d31131f3d405a2ced5e5
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2020-05-26 03:54:33 +0000

    Whitespace: four spaces instead of one
    Long ago I thought one space was plenty readable,
    and if you are used to it, and look at it on the
    right screen at the right distance with the size
    and font good enough, and have good vision, yeah,
    it is. But it is worse along several objective
    dimensions of readability.

--- fdtee.c
+++ fdtee.c
@@ -36,15 +36,15 @@ The most significant bit of an unsigned char.
 \*/
 
 char const helpText[] =
- "\n"
- "Usage: fdtee [OPTION] [FD]...\n"
- "\n"
- "Copy stdin to each FD (file descriptor)*.\n"
- "\n"
- "  -h, --help              Print this help text and exit.\n"
- "  -i, --ignore-interrupts Ignore interrupt signals (SIGINT).\n"
- "\n"
- " * File descriptors are expected to be positive integers.\n"
+    "\n"
+    "Usage: fdtee [OPTION] [FD]...\n"
+    "\n"
+    "Copy stdin to each FD (file descriptor)*.\n"
+    "\n"
+    "  -h, --help              Print this help text and exit.\n"
+    "  -i, --ignore-interrupts Ignore interrupt signals (SIGINT).\n"
+    "\n"
+    " * File descriptors are expected to be positive integers.\n"
 ;
 
 char const unrecognizedOption[] = "fdtee: Unrecognized option: ";
@@ -53,124 +53,124 @@ char const fdOverflowedInt[] = "fdtee: FD value greater than maximum possible: "
 #define bytepack_m(ptr, val) \
 if(!val) \
 { \
- ptr[0] = UCHAR_TOP_BIT; \
- ptr[1] = 0; \
+    ptr[0] = UCHAR_TOP_BIT; \
+    ptr[1] = 0; \
 } \
 else \
 { \
- do \
- { \
-  unsigned char bytepack_m_tempUChar = val; \
-  val >>= CHAR_BIT - 1; \
-  if(val) \
-  { \
-   bytepack_m_tempUChar |= UCHAR_TOP_BIT; \
-  } \
-  *ptr = bytepack_m_tempUChar; \
-  ptr += 1; \
- } \
- while(val); \
+    do \
+    { \
+        unsigned char bytepack_m_tempUChar = val; \
+        val >>= CHAR_BIT - 1; \
+        if(val) \
+        { \
+            bytepack_m_tempUChar |= UCHAR_TOP_BIT; \
+        } \
+        *ptr = bytepack_m_tempUChar; \
+        ptr += 1; \
+    } \
+    while(val); \
 }
 
 #define byteback_m(ptr, val) \
 for \
 ( \
- unsigned char byteback_m_shift = 0, \
-  byteback_m_continueBit = UCHAR_TOP_BIT; \
- byteback_m_continueBit; \
- byteback_m_shift += CHAR_BIT - 1, ptr += 1 \
+    unsigned char byteback_m_shift = 0, \
+        byteback_m_continueBit = UCHAR_TOP_BIT; \
+    byteback_m_continueBit; \
+    byteback_m_shift += CHAR_BIT - 1, ptr += 1 \
 ) \
 { \
- unsigned char byteback_m_tempUChar = *ptr; \
- byteback_m_continueBit &= byteback_m_tempUChar; \
- byteback_m_tempUChar &= UCHAR_HALF_MAX; \
- val += byteback_m_tempUChar << byteback_m_shift; \
+    unsigned char byteback_m_tempUChar = *ptr; \
+    byteback_m_continueBit &= byteback_m_tempUChar; \
+    byteback_m_tempUChar &= UCHAR_HALF_MAX; \
+    val += byteback_m_tempUChar << byteback_m_shift; \
 }
 
 #define strToFD_m(str, val) \
 val = 0; \
 for(int c = *str; c; str += 1, c = *str) \
 { \
- if(c < '0' || c > '9') \
- { \
-  val = -1; \
-  break; \
- } \
- if(val <= INT_MAX / 10) \
- { \
-  val *= 10; \
-  c -= '0'; \
-  if(val <= INT_MAX - c) \
-  { \
-   val += c; \
-   continue; \
-  } \
- } \
- write(2, fdOverflowedInt, sizeof(fdOverflowedInt) - 1); \
- write(2, argPtr2, strlen(argPtr2)); \
- return EXIT_FAILURE; \
+    if(c < '0' || c > '9') \
+    { \
+        val = -1; \
+        break; \
+    } \
+    if(val <= INT_MAX / 10) \
+    { \
+        val *= 10; \
+        c -= '0'; \
+        if(val <= INT_MAX - c) \
+        { \
+            val += c; \
+            continue; \
+        } \
+    } \
+    write(2, fdOverflowedInt, sizeof(fdOverflowedInt) - 1); \
+    write(2, argPtr2, strlen(argPtr2)); \
+    return EXIT_FAILURE; \
 }
 
 #define handleOption_m(str) \
 if(!strcmp(str, "-i") || !strcmp(str, "--ignore-interrupts")) \
 { \
- signal(SIGINT, SIG_IGN); \
+    signal(SIGINT, SIG_IGN); \
 } \
 else \
 if(!strcmp(str, "-h") || !strcmp(str, "--help")) \
 { \
- write(1, helpText + 1, sizeof(helpText) - 2); \
- return EXIT_SUCCESS; \
+    write(1, helpText + 1, sizeof(helpText) - 2); \
+    return EXIT_SUCCESS; \
 } \
 else \
 { \
- write(2, unrecognizedOption, sizeof(unrecognizedOption) - 1); \
- write(2, str, strlen(str)); \
- write(2, helpText, sizeof(helpText) - 1); \
- return EXIT_FAILURE; \
+    write(2, unrecognizedOption, sizeof(unrecognizedOption) - 1); \
+    write(2, str, strlen(str)); \
+    write(2, helpText, sizeof(helpText) - 1); \
+    return EXIT_FAILURE; \
 }
 
 int main(int argc, char * * argv)
 {
- int fd;
- /* If there are arguments, the first argument is the program name: skip it. */
- for(size_t i = 1; i < argc; i += 1)
- {
-  char * restrict argPtr1 = argv[i];
-  char * restrict argPtr2 = argPtr1;
-  strToFD_m(argPtr1, fd)
-  if(fd >= 0 && argPtr1 > argPtr2)
-  {
-   bytepack_m(argPtr2, fd);
-   continue;
-  }
-  handleOption_m(argPtr2)
-  *argPtr2 = 0;
- }
+    int fd;
+    /* If there are arguments, the first argument is the program name: skip it. */
+    for(size_t i = 1; i < argc; i += 1)
+    {
+        char * restrict argPtr1 = argv[i];
+        char * restrict argPtr2 = argPtr1;
+        strToFD_m(argPtr1, fd)
+        if(fd >= 0 && argPtr1 > argPtr2)
+        {
+            bytepack_m(argPtr2, fd);
+            continue;
+        }
+        handleOption_m(argPtr2)
+        *argPtr2 = 0;
+    }
  
- #ifdef SIGPIPE
-  signal(SIGPIPE, SIG_IGN);
- #endif
+    #ifdef SIGPIPE
+        signal(SIGPIPE, SIG_IGN);
+    #endif
  
- char buffer[PIPE_BUF];
- ssize_t readcount;
- int exitstatus = EXIT_SUCCESS;
- while((readcount = read(0, &buffer, PIPE_BUF)) > 0)
- {
-  for(size_t i = 1; i < argc; i += 1)
-  {
-   unsigned char * restrict arg = argv[i];
-   if(!*arg)
-   {
-    continue;
-   }
-   fd = 0;
-   byteback_m(arg, fd)
-   if(write(fd, &buffer, readcount) != readcount)
-   {
-    exitstatus = EXIT_FAILURE;
-   }
-  }
- }
- return exitstatus;
+    char buffer[PIPE_BUF];
+    ssize_t readcount;
+    int exitstatus = EXIT_SUCCESS;
+    while((readcount = read(0, &buffer, PIPE_BUF)) > 0)
+    {
+        for(size_t i = 1; i < argc; i += 1)
+        {
+            unsigned char * restrict arg = argv[i];
+            if(!*arg)
+            {
+                continue;
+            }
+            fd = 0;
+            byteback_m(arg, fd)
+            if(write(fd, &buffer, readcount) != readcount)
+            {
+                exitstatus = EXIT_FAILURE;
+            }
+        }
+    }
+    return exitstatus;
 }
